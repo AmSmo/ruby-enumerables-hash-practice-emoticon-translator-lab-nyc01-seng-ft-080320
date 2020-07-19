55cb470807267d2018205e5f0548e3eae3fcf776
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-19 20:49:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file, emoticon)
     lib[emoticon][:japanese]
 end
 
-def get_english_meaning
+def get_english_meaning(file, emoticon)
   # code goes here
   lib = load_library(file)
     lib[emoticon][:english]
